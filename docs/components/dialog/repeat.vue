0913d17c168823bb938c4fd02c0c306ef8bdba4c
leemotive <wstation@163.com>
2025-07-09 21:11:46 +0800

    docs: 无限等待

--- docs/components/dialog/repeat.vue
+++ docs/components/dialog/repeat.vue
@@ -1,5 +1,7 @@
 <template>
   <ElButton type="primary" @click="openDialog">打开弹窗</ElButton>
+  <ElButton type="primary" @click="openDialog2">打开弹窗</ElButton>
+
   <TdDialog ref="dialog" title="标题" :show-close="false">
     <div>内容</div>
     <template #footer="scope">
@@ -18,6 +20,7 @@ const dialogRef = useTemplateRef('dialog');
 async function openDialog() {
   const { close } = dialogRef.value!.open();
 
+  // 使用循环
   while (true) {
     await dialogRef.value!.wait();
     const { value } = await ElMessageBox.prompt(
@@ -30,6 +33,25 @@ async function openDialog() {
     }
   }
   // 后续其它处理逻辑
+}
+
+async function openDialog2() {
+  const { close } = dialogRef.value!.open();
+
+  await afterClose();
+
+  // 使用递归
+  async function afterClose() {
+    await dialogRef.value!.wait();
+    const { value } = await ElMessageBox.prompt(
+      '自定义操作，比如提交表单，1：成功并关闭弹窗，0：失败不关闭弹窗',
+      '提示',
+    );
+    if (value !== '1') {
+      return afterClose();
+    }
+    close();
+  }
 }
 </script>
 
